a169949d1c2f18ff0971d418b6a655e28be1bc58
Takao-Koyanagi <clarkoya0730@gmail.com>
2025-01-11 19:26:56 +0900

    商品を登録できるようにする

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: :index
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :items, only: [:new, :create]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
