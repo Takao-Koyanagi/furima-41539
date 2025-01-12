cefd37748c6772effc4ea7739a24501365305e5b
Takao-Koyanagi <clarkoya0730@gmail.com>
2025-01-12 17:05:11 +0900

    金額入力時に販売手数料や販売利益の計算結果が表示されるようにする

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,13 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :shipping_cost_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :shipping_day_id, numericality: { other_than: 1 }
+  validates :price, presence: true,
+                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },
+                    format: { with: /\A[0-9]+\z/ }
+
 end
